731e689e4553c68a10fbe83166aba154d2c5a266
gautam sharma <gautamsh@leewayhertz.com>
2022-08-09 15:58:46 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@
 import './App.css';
 import SideBar from "./modules/SideBar";
 import Chat from "./modules/Chat";
-
+import { useEffect, useState } from 'react';
+import Pusher from "pusher-js";
+import axios from "./axios"
 function App() {
+  const [messages,setMessage]=useState([]);
+  useEffect(()=>{
+    
+    axios.get('/message/sync').then(response=>{
+      setMessage(response.data);
+      console.log(response.data)
+    })
+  },[])
+
+  useEffect(()=>{
+    var pusher = new Pusher('6a88528e834b4f387dcf', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) =>{
+      alert(JSON.stringify(newMessage));
+      setMessage([...messages,newMessage])
+    });
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  },[messages])
+
+
   return (
     <div className="app">
       <div className='app_body'>
       <SideBar/>
-      <Chat/>
+      <Chat messages={messages}/>
       </div>
     </div>
   );
--- src/modules/Chat.js
+++ src/modules/Chat.js
@@ -6,63 +6,63 @@ import InsertEmotionIcon from "@material-ui/icons/InsertEmoticon";
 import MicICon from "@material-ui/icons/Mic";
 import React from "react";
 import "../assets/styles/custom.css";
+import {useState} from "react";
+import axios from "axios";
 
-function Chat() {
-  return <>
-  <div className="chat">
-    <div className="chat_header">
-      <Avatar />
-      <div className="chat_headerInfo">
-        <h3>Room name</h3>
-        <p>Last Seen at...</p>
+function Chat(props) {
+  const [input ,setInput]=useState("");
+  const send=(e)=>{
+    e.preventDefault();
+    axios.post("message/new",{
+      message:input,
+      name:"demo app",
+      timestamp:'just now',
+      recevied:false,
+    })
+  }
+  return (
+    <>
+      <div className="chat">
+        <div className="chat_header">
+          <Avatar />
+          <div className="chat_headerInfo">
+            <h3>Room name</h3>
+            <p>Last Seen at...</p>
+          </div>
+          <div className="chat_headerRight">
+            <IconButton>
+              <SearchOutlined />
+            </IconButton>
+            <IconButton>
+              <AttatchFile />
+            </IconButton>
+            <IconButton>
+              <MoreVert />
+            </IconButton>
+          </div>
+        </div>
+
+        <div className="chat_body">
+          {props?.messages.map((ele) => (
+            <p className={`chat_message ${ele.recevied && "chat_receiver"}`}>
+              <span className="chat_name">{ele.name}</span>
+              {ele.message}
+              <span className="chat_timestamp">{ele.timestamp}</span>
+            </p>
+          ))}
+        </div>
+        <div className="chat_footer">
+          <InsertEmotionIcon />
+          <form>
+            <input value={input} onChange={(e)=>setInput(e.target.value)} placeholder={"Type a message"} type="text" />
+            <button type="submit" onClick={send}>send a message</button>
+          </form>
+          <MicICon></MicICon>
+        </div>
       </div>
-      <div className="chat_headerRight">
-        <IconButton>
-          <SearchOutlined />
-        </IconButton>
-        <IconButton>
-          <AttatchFile />
-        </IconButton>
-        <IconButton>
-          <MoreVert />
-        </IconButton>
-
-      </div>
-    </div>
-
-    <div className="chat_body">
-      <p className="chat_message">
-        <span className="chat_name">
-          gauta,
-        </span>
-        This is a message
-        <span className="chat_timestamp">{new Date().toUTCString()}</span>
-      </p>
-
-      <p className="chat_message chat_receiver">
-        <span className="chat_name">
-          gauta,
-        </span>
-        This is a message
-        <span className="chat_timestamp">{new Date().toUTCString()}</span>
-      </p>
-
-      
-    </div>
-    <div className="chat_footer">
-        <InsertEmotionIcon />
-        <form>
-          <input
-         
-          placeholder={"Type a message"}
-          type="text"
-          />
-          <button >send a message</button>
-        </form>
-        <MicICon></MicICon>
-      </div>
-    </div>;
-  </>
+      ;
+    </>
+  );
 }
 
 export default Chat;
